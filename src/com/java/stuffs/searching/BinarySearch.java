36bbe72a6d9051408fab5e2a47173bfec987ee6c
kenyinka <ken.okuneye@gmail.com>
2016-09-13 17:14:53 -0400

    Update

--- src/com/java/stuffs/searching/BinarySearch.java
+++ src/com/java/stuffs/searching/BinarySearch.java
@@ -9,7 +9,7 @@ public class BinarySearch {
     public static void main (String[] argx) {
         int[] a = new int [] {10, 15, 19, 24, 27, 30, 32, 33, 35, 45, 50, 53, 64, 74, 76, 82, 90, 93, 133, 205, 552, 563};
         int index = -1;
-        int searched = 133;
+        int searched = 10;
         int lower = 0;
         int higher = a.length - 1;
         int mid = Math.abs((higher + lower)/ 2);
@@ -25,7 +25,7 @@ public class BinarySearch {
                 lower = mid + 1;
                 mid = Math.abs((higher + lower)/ 2);
             } else if (searched < a[mid]) {
-                higher = mid + 1;
+                higher = mid;
                 mid = Math.abs((higher + lower)/ 2);
             } else if (a[mid] == searched) {
                 index = mid;
